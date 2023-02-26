57795fd3545d208d71605b24127b1f96989fedf0
S.Miah <serhanmiah@icloud.com>
2023-02-26 07:19:10 +0000

    change to heroku settings

---  wsgi.py
+++  wsgi.py
@@ -1,9 +1,12 @@
 import os
 from project import create_app
 from waitress import serve
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    serve(app, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
