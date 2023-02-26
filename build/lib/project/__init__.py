85698cba7d9df3f7ae81323dd6c968b04dab2c9f
S.Miah <serhanmiah@icloud.com>
2023-02-26 21:24:24 +0000

    redoing settings

--- build/lib/project/__init__.py
+++ build/lib/project/__init__.py
@@ -4,8 +4,6 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from project.config import Config
-from sass import compile
-# from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -14,28 +12,21 @@ login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
 
-
-
 def create_app(config_class=Config):
     app = Flask(__name__)
-    # Bootstrap(app)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
 
-    
     from project.users.routes import users
     from project.main.routes import main
     from project.job_board.routes import job_board
     from project.errors.handlers import errors
-    # Move the import inside the create_app function
     from project.applicants.routes import applicants_bp
 
-
-    
     app.register_blueprint(users)
     app.register_blueprint(main)
     app.register_blueprint(job_board)
