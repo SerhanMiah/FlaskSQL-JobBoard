7534a41860603662c7a7efe9bacecbb71bd58187
S.Miah <serhanmiah@icloud.com>
2023-02-26 00:22:22 +0000

    new wsgi file

---  wsgi.py
+++  wsgi.py
@@ -1,4 +1,9 @@
+import os
 from project import create_app
-
+from waitress import serve
 
 app = create_app()
+
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
