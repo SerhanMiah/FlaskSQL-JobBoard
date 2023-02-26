e738baa290b071c2b8a8b3a734678b9a0a61d08e
S.Miah <serhanmiah@icloud.com>
2023-02-26 10:06:37 +0000

    railway settings:

--- project/config.py
+++ project/config.py
@@ -3,8 +3,9 @@ import os
 
 load_dotenv()
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' or \
+    #     'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
 
